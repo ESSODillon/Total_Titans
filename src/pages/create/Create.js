4b76a2acfca68246b27c1c3e3821e631a4bba236
Dillon Polley <65629239+ESSODillon@users.noreply.github.com>
2022-01-23 16:42:10 -0500

    Create workout feature

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,8 +1,169 @@
+import React, { useRef } from "react";
+import { useState } from "react";
+
 // Styles
 import "./Create.css";
 
-import React from "react";
-
 export default function Create() {
-  return <div>Create</div>;
+  const [title, setTitle] = useState("");
+  const [rest, setRest] = useState("");
+  const [reps, setReps] = useState("");
+  const [about, setAbout] = useState("");
+  const [experience, setExperience] = useState("");
+
+  // Equipment state
+  const [newEquipment, setNewEquipment] = useState("");
+  const [equipment, setEquipment] = useState([]);
+  const equipmentInput = useRef(null);
+
+  // Exercises state
+  const [newExercises, setNewExercises] = useState("");
+  const [exercises, setExercises] = useState([]);
+  const exerciseInput = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(title, about, experience, equipment, exercises);
+  };
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+
+    const target = e.target.className;
+
+    if (target == "btn-equipment") {
+      const equip = newEquipment.trim();
+
+      if (equip && !equipment.includes(equip)) {
+        setEquipment((prevEquipment) => [...prevEquipment, equip]);
+      }
+
+      setNewEquipment("");
+      equipmentInput.current.focus();
+    }
+
+    if (target == "btn-exercises") {
+      const exc = newExercises.trim();
+
+      if (exc && !exercises.includes(exc)) {
+        setExercises((prevExercises) => [...prevExercises, exc]);
+      }
+
+      setNewExercises("");
+      exerciseInput.current.focus();
+    }
+  };
+
+  return (
+    <div className="create">
+      <h2 className="page-title">Add a New Workout</h2>
+
+      <form onSubmit={handleSubmit}>
+        <div className="row">
+          <div className="column">
+            <label className="workout-input">
+              <span>Workout title:</span>
+              <input
+                type="text"
+                onChange={(e) => setTitle(e.target.value)}
+                value={title}
+                required
+              />
+            </label>
+
+            {/* Equipment go here */}
+
+            <label>
+              <span>Workout equipment:</span>
+              <div className="equipment">
+                <input
+                  type="text"
+                  name="equipment"
+                  onChange={(e) => setNewEquipment(e.target.value)}
+                  value={newEquipment}
+                  ref={equipmentInput}
+                />
+                <button onClick={handleAdd} className="btn-equipment">
+                  add
+                </button>
+              </div>
+            </label>
+            <p>
+              Current equipment:{" "}
+              {equipment.map((i) => (
+                <em key={i}>{i}, </em>
+              ))}
+            </p>
+
+            <label>
+              <span>Reps:</span>
+              <input
+                type="text"
+                onChange={(e) => setReps(e.target.value)}
+                value={reps}
+                required
+              />
+            </label>
+          </div>
+          <div className="column">
+            <label>
+              <span>Rest between sets:</span>
+              <input
+                type="text"
+                onChange={(e) => setRest(e.target.value)}
+                value={rest}
+                required
+              />
+            </label>
+            {/* Exercises go here */}
+
+            <label>
+              <span>Workout exercises:</span>
+              <div className="exercises">
+                <input
+                  type="text"
+                  name="equipment"
+                  onChange={(e) => setNewExercises(e.target.value)}
+                  value={newExercises}
+                  ref={exerciseInput}
+                />
+                <button onClick={handleAdd} className="btn-exercises">
+                  add
+                </button>
+              </div>
+            </label>
+            <p>
+              Current exercises:{" "}
+              {exercises.map((i) => (
+                <em key={i}>{i}, </em>
+              ))}
+            </p>
+
+            <label>
+              <span>Difficulty:</span>
+              <input
+                type="text"
+                onChange={(e) => setExperience(e.target.value)}
+                value={experience}
+                required
+              />
+            </label>
+          </div>
+        </div>
+
+        <label>
+          <span>Description:</span>
+          <textarea
+            onChange={(e) => setAbout(e.target.value)}
+            value={about}
+            required
+          />
+        </label>
+
+        <br />
+
+        <button className="btn">submit</button>
+      </form>
+    </div>
+  );
 }
