262fefa3abdd7ee8d0f8e25727bea7ead0b64ca5
Dillon Polley <65629239+ESSODillon@users.noreply.github.com>
2022-01-23 14:30:00 -0500

    Fetching a single workout

--- src/pages/workout/Workout.js
+++ src/pages/workout/Workout.js
@@ -1,8 +1,20 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useFetch } from "../../hooks/useFetch";
+
 // Styles
 import "./Workout.css";
 
-import React from "react";
-
 export default function Workout() {
-  return <div>Workout</div>;
+  const { id } = useParams();
+  const url = "http://localhost:3000/workouts/" + id;
+  const { data: workout, isPending, error } = useFetch(url);
+
+  return (
+    <div className="workout">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {workout && <h1>{workout.title}</h1>}
+    </div>
+  );
 }
