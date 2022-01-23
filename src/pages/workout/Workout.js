69b24dd396c05b24473f1e7f581e933bcf7cc38d
Dillon Polley <65629239+ESSODillon@users.noreply.github.com>
2022-01-23 15:08:32 -0500

    Individual workout details

--- src/pages/workout/Workout.js
+++ src/pages/workout/Workout.js
@@ -14,7 +14,30 @@ export default function Workout() {
     <div className="workout">
       {error && <p className="error">{error}</p>}
       {isPending && <p className="loading">Loading...</p>}
-      {workout && <h1>{workout.title}</h1>}
+      {workout && (
+        <>
+          <h2 className="page-title">{workout.title}</h2>
+          <p className="experience">
+            Recommended for level: {workout.experience}
+          </p>
+          <ul className="equipment">
+            <span>Equipment: </span>
+            {workout.equipment.map((equip) => (
+              <li key={equip}>{equip}</li>
+            ))}
+          </ul>
+          <ul className="exercises">
+            <span>Exercises: </span>
+            {workout.exercises.map((exc) => (
+              <li key={exc}>{exc}</li>
+            ))}
+          </ul>
+
+          <p className="reps">Reps: {workout.reps}</p>
+          <p className="rest">Reps: {workout.rest}</p>
+          <p className="about">{workout.about}</p>
+        </>
+      )}
     </div>
   );
 }
