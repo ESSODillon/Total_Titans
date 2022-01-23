57766da2f4c5385fd7ec91ba57e74966739e01da
Dillon Polley <65629239+ESSODillon@users.noreply.github.com>
2022-01-23 13:47:12 -0500

    Fetching data

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,18 @@
+import { useFetch } from "../../hooks/useFetch";
+
 // Styles
 import "./Home.css";
 
 import React from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  const { data, isPending, error } = useFetch("http://localhost:3000/workouts");
+
+  return (
+    <div className="home">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {data && data.map((workout) => <h2 key={workout.id}>{workout.title}</h2>)}
+    </div>
+  );
 }
