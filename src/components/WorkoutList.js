d518b67f9069b9e2d4150eea8861e152e9e8989f
Dillon Polley <65629239+ESSODillon@users.noreply.github.com>
2022-01-24 09:42:57 -0500

    Delete functionality

--- src/components/WorkoutList.js
+++ src/components/WorkoutList.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useTheme } from "../hooks/useTheme";
+import Trashcan from "../assets/trashcan.svg";
+import { projectFirestore } from "../firebase/config";
 
 // Styles
 import "./WorkoutList.css";
@@ -11,6 +13,11 @@ export default function WorkoutList({ workouts }) {
   if (workouts.length == 0) {
     return <div className="error">No workouts to load...</div>;
   }
+
+  const deleteItem = (id) => {
+    projectFirestore.collection("workouts").doc(id).delete();
+  };
+
   return (
     <div className="workout-list">
       {workouts.map((workout) => (
@@ -19,6 +26,11 @@ export default function WorkoutList({ workouts }) {
           <p>Difficulty level: {workout.experience}</p>
           <div>{workout.about.substring(0, 100)}...</div>
           <Link to={`/Total_Titans/workouts/${workout.id}`}>Read More</Link>
+          <img
+            className="delete"
+            src={Trashcan}
+            onClick={() => deleteItem(workout.id)}
+          />
         </div>
       ))}
     </div>
