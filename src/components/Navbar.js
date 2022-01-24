9c53d0d983cdb527f4deb97f017cba22523430a3
Dillon Polley <65629239+ESSODillon@users.noreply.github.com>
2022-01-23 21:41:46 -0500

    Custom Context Hook

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,17 @@
 import { Link } from "react-router-dom";
+import { useTheme } from "../hooks/useTheme";
 
 // Styles
 import "./Navbar.css";
 
+// Components
 import Searchbar from "./Searchbar";
 
 export default function Navbar() {
+  const { color } = useTheme();
+
   return (
-    <div className="navbar">
+    <div className="navbar" style={{ background: color }}>
       <nav>
         <Link to="/" className="brand">
           <h1>Total Titans</h1>
